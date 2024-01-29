bd918fb0ac0242a6ace14711961f237aa335a624
Afi-THEdeveloper <mohdafsal1049@gmail.com>
2024-01-29 12:45:14 +0530

    updated api base url to hosted render backend

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:5000' 
+// const API_BASE_URL = 'http://localhost:5000' 
+const API_BASE_URL = 'https://eventsphere-backend.onrender.com' 
 
 
 export default {
