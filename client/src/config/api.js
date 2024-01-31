1afda201dc92536268d11b118dd6d83cf017d145
Afi-THEdeveloper <mohdafsal1049@gmail.com>
2024-01-31 17:12:25 +0530

    commit for avoid domain

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,7 +2,7 @@
 // export  const API_BASE_URL = 'http://localhost:5000'
 
 // production
-export  const API_BASE_URL = 'http://3.108.145.99'
-// export const API_BASE_URL = 'https://eventsphere-backend.onrender.com' 
+// export  const API_BASE_URL = 'http://3.108.145.99'
+export const API_BASE_URL = 'https://eventsphere-backend.onrender.com' 
 
 
