cf85e1d18df53bd1644dfbd1e3b6f5974e86ebe4
Afi-THEdeveloper <mohdafsal1049@gmail.com>
2024-02-01 18:15:37 +0530

    forget to change base url from frontend to live

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,8 @@
 // building 
-export  const API_BASE_URL = 'http://localhost:5000'
+// export  const API_BASE_URL = 'http://localhost:5000'
 
 // production
-// export  const API_BASE_URL = 'https://eventsphere.online'
+export  const API_BASE_URL = 'https://eventsphere.online'
 // export const API_BASE_URL = 'https://eventsphere-backend.onrender.com' 
 
 
